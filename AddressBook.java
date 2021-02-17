f9847f839fb3f723401701ed7c0aca7a6c962df4
MoreKajal <kajalm3105@gmail.com>
2021-02-17 08:51:28 -0800

    [Kajal]Added:Code to Ask User For Details

--- AddressBook.java
+++ AddressBook.java
@@ -1,42 +1,40 @@
 package addressbook.com;
+import java.util.Scanner;
 
 public class AddressBook {
 	
-	public String firstName;
-	public String lastName;
-	public String address;
-	public String city;
-	public String state;
-	public int zipCode;
-	public String phoneNumber;
-	public String email;
-	
-	//Constructor
-	public AddressBook(String firstName, String lastName, String address, String city, 
-			String state, int zipCode, String phoneNumber, String email) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-		this.city = city;
-		this.state = state;
-		this.zipCode = zipCode;
-		this.phoneNumber = phoneNumber;
-		this.email = email;
+	public void AddYourDetails() {
+		Scanner input = new Scanner(System.in);
+		System.out.println(" \n Enter your First Name");
+		String firstName = input.next();
+		System.out.println("\n Enter your Last Name");
+		String lastName = input.next();
+		System.out.println("\n Enter your Address");
+		String address = input.next();
+		System.out.println("\n Enter your City name");
+		String city = input.next();
+		System.out.println("\n Enter your State name");
+		String state = input.next();
+		System.out.println("\n Enter your Zip Code");
+		String zipCode = input.next();
+		System.out.println("\n Enter your Phone number");
+		String phoneNumber = input.next();
+		System.out.println("\n Enter your Email address");
+		String email = input.next();
+		System.out.println("First Name: " + firstName);
+		System.out.println("Last Name: " + lastName);
+		System.out.println("House Number: " + address);
+		System.out.println("City: " + city);
+		System.out.println("State: " + state);
+		System.out.println("Pin Code: " + zipCode);
+		System.out.println("Phone Number: " + phoneNumber);
+		System.out.println("E-Mail: " + email);
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("***Welcome to Address Book Program***");
-		AddressBook makeEntry = new AddressBook("Kajal", "More", "Karmveer_Nagar", "Shirdi", 
-				"Maharashtra", 423603, "9270371777", "kajalm3105@gmail.com");
-		System.out.println("First name: " + makeEntry.firstName);
-		System.out.println("Last name: " + makeEntry.lastName);
-		System.out.println("Address: " + makeEntry.address);
-		System.out.println("City: " + makeEntry.city);
-		System.out.println("State: " + makeEntry.state);
-		System.out.println("Zip code: " + makeEntry.zipCode);
-		System.out.println("Phone number: " + makeEntry.phoneNumber);
-		System.out.println("Email: " + makeEntry.email);
+		System.out.println("WELCOME to Address Book Program");
+		AddressBook contact = new AddressBook();
+		contact.AddYourDetails();
 		
 	}
-
 }
